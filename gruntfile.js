ca28244e08e1121f3ab3695e46ac0dc0903678ba
Peter DHaiti <dsonson23@hotmail.com>
2016-12-31 00:22:52 -0500

    Started configuring gruntfile.js

--- gruntfile.js
+++ gruntfile.js
@@ -10,14 +10,54 @@ module.exports = function(grunt) {
       build: {
         src: 'src/js/*.js',
         dest: 'js/yeap.min.js'
+      },
+      dev: {
+        options: {
+          beautify: true,
+          mangle: false,
+          compress: false,
+          preserveComments: 'all'
+        },
+        src: 'src/js/*.js',
+        dest: 'js/pd.yeap.min.js'   
+      },
+      sass: {
+         dev: {
+          options: {
+             outputStyle: 'expanded'
+          },
+          files: {
+            'css/pd.yeap.css' : 'src/css/pd.yeap.scss'
+          }
+         },
+         build: {
+          options: {
+             outputStyle: 'compressed'
+          },
+          files: {
+            'css/pd.yeap.min.css' : 'src/css/pd.yeap.scss'
+          }
+         }
+      },
+    watch: {
+      js: {
+        files: ['src/js/*.js']
+        tasks: ['uglify:dev']
+      },
+      css: {
+        files: ['src/css/*.scss']
+        tasks: ['sass:dev']
       }
     }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-sass');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['uglify:dev']);
+  grunt.registerTask('build', ['uglify:build']);
 
 };

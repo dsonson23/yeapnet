4af7176d3a9350ac42303dfafd2f49f8ef6bd92d
Peter DHaiti <dsonson23@hotmail.com>
2017-02-12 08:41:25 -0500

    Created the free account button on the video background header and deleted some unwanted js files

--- include/footer.js
+++
@@ -1,7 +0,0 @@
-<article class="container-fluid">        		
-        		<div class="row">
-	        		<div class="col-xs-12 text-center">
-	        			<p>@ Yeap Network 2017, all right reserved, developed by <a href="http://dhaiti.pro/" target="-blank">Peter DHaiti</a></p>
-	        		</div>
-        		</div>
-			</article>       

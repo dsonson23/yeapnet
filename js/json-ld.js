5ba11997a944f2bc8a2c375fe8928aadcf7d99fe
Peter DHaiti <dsonson23@hotmail.com>
2017-02-13 14:05:18 -0500

    Edited structured data, edit the http request to be compatible with https, added redirect code on htaccess file to redirect http to https

---
+++ js/json-ld.js
@@ -0,0 +1,27 @@
+{
+  "@context": "http://schema.org/",
+  "@type": "Product",
+   "url": "http://yeapnetwork.net",
+  "logo": "http://yeapnetwork.net/img/logo.jpg",
+  "name": "Build your own web hosting  business and services",
+  "image": "http://yeapnetwork.net/img/data-center.jpg",
+  "description": "Yeap Network provides you with everything you need to become a web hosting reseller and services without owning a data center.",
+  "brand": {
+    "@type": "Product",
+    "name": "Yeap Reseller"
+  },
+  "aggregateRating": {
+    "@type": "AggregateRating",
+    "ratingValue": "5.0",
+    "reviewCount": "89"
+  },
+  "offers": {
+    "@type": "Offer",
+    "priceCurrency": "USD",
+    "price": "199.00",
+    "seller": {
+      "@type": "Product",
+      "name": "Domain names, web hosting and services reseller plans"
+    }
+  }
+}
